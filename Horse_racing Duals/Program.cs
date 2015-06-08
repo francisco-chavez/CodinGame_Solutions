ca0f32972722053a9e64b42261a01e8aea3e6eff
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-06-07 19:22:59 -0700

    Adding my solution for "Horse-racing Duals" exercise.

---
+++ Horse_racing Duals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+	static void Main(string[] args)
+	{
+		int N = int.Parse(Console.ReadLine());
+		List<int> strengths = new List<int>(N);
+		for (int i = 0; i < N; i++)
+		{
+			int Pi = int.Parse(Console.ReadLine());
+			strengths.Add(Pi);
+		}
+		strengths.Sort();
+		int deltaMin = strengths[1] - strengths[0];
+		for (int i = 2; i < strengths.Count; i++)
+		{
+			var delta = strengths[i] - strengths[i - 1];
+			deltaMin = Math.Min(deltaMin, delta);
+		}
+
+		// Write an action using Console.WriteLine()
+		// To debug: Console.Error.WriteLine("Debug messages...");
+
+		Console.WriteLine(deltaMin);
+	}
+}
