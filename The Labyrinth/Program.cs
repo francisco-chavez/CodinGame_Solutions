2fbd6de9106ceef9894aaeebe6c896ff846b8476
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-06-10 16:03:20 -0700

    Updated given code template.
    * I renamed some of the variables to have more informative names.
    * I added a few comments to help me remember what the different
    characters remember what the possible maze cell states mean.
    * Added ability to keep track of maze state in local memory.

--- The Labyrinth/Program.cs
+++ The Labyrinth/Program.cs
@@ -15,19 +15,30 @@ class Player
 	{
 		string[] inputs;
 		inputs = Console.ReadLine().Split(' ');
-		int R = int.Parse(inputs[0]); // number of rows.
-		int C = int.Parse(inputs[1]); // number of columns.
-		int A = int.Parse(inputs[2]); // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
+		int rowCount		= int.Parse(inputs[0]);	// number of rows.
+		int columnCount		= int.Parse(inputs[1]); // number of columns.
+		int alarmCountDown	= int.Parse(inputs[2]); // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
 
 		// game loop
+		char[,] maze = new char[columnCount, rowCount];
+
 		while (true)
 		{
+			// '#' : { Wall }
+			// '.' : { Hollow space }
+			// 'T' : { Starting position }
+			// 'C' : { Control room }
+			// '?' : { Un-scanned cell }
+
 			inputs = Console.ReadLine().Split(' ');
-			int KR = int.Parse(inputs[0]); // row where Kirk is located.
-			int KC = int.Parse(inputs[1]); // column where Kirk is located.
-			for (int i = 0; i < R; i++)
+			int currentRow		= int.Parse(inputs[0]); // row where Kirk is located.
+			int currentColumn	= int.Parse(inputs[1]); // column where Kirk is located.
+
+			for (int i = 0; i < rowCount; i++)
 			{
 				string ROW = Console.ReadLine(); // C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
+				for (int j = 0; j < columnCount; j++)
+					maze[i, j] = ROW[j];
 			}
 
 			// Write an action using Console.WriteLine()
